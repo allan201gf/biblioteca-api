750a2f94365c6388f0d99df45ad9bee1e46b183e
allan201gf <allan201gf@gmail.com>
2021-09-21 14:30:09 -0300

    Imports organizados

--- src/main/java/br/com/allangf/bibliotecaapi/BibliotecaApiApplication.java
+++ src/main/java/br/com/allangf/bibliotecaapi/BibliotecaApiApplication.java
@@ -2,10 +2,6 @@ package br.com.allangf.bibliotecaapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class BibliotecaApiApplication {
